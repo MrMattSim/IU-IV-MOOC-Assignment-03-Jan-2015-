86b1cefec5dc5d80d14ec7edd0f0122be7e42c14
Bob Rudis <bob@rudis.net>
2015-01-28 19:59:54 -0500

    Updated code/readme

--- R/a03.R
+++ R/a03.R
@@ -9,23 +9,36 @@ library(ggplot2)
 library(statebins)
 library(gridExtra)
 
-# for theme_map
+# For the map ggplot theme ------------------------------------------------
+
 devtools::source_gist("33baa3a79c5cfef0f6df")
+
+# Read in NSF data --------------------------------------------------------
+
+dat <- read.csv("data/NSF_master_table.csv", stringsAs=FALSE)
 
 data(zip_codes)
 
-dat <- read.csv("data/NSF_master_table.csv", stringsAs=FALSE)
+# Restrict records to those with valid states -----------------------------
 
 dat %>%
   filter(dat$state != "  ") %>%
   select(year=published_year, STUSPS=state) -> dat
 
+# First aggregration ------------------------------------------------------
+
 dat %>% count(year, STUSPS) -> by_state
+
+# Get shapefile -----------------------------------------------------------
 
 us <- readOGR(dsn="data/state.geojson", "OGRGeoJSON")
 
+# Convert to Winkel-Tripel projection -------------------------------------
+
 us_aea <- spTransform(us, CRS("+proj=laea +lat_0=45 +lon_0=-100 +x_0=0 +y_0=0 +a=6370997 +b=6370997 +units=m +no_defs"))
 us_aea@data$id <- rownames(us_aea@data)
+
+# Move some states/territories around -------------------------------------
 
 alaska <- us_aea[us_aea$STATEFP=="02",]
 alaska <- elide(alaska, rotate=-50)
@@ -45,11 +58,16 @@ proj4string(puerto_rico) <- proj4string(us_aea)
 us_aea <- us_aea[!us_aea$STATEFP %in% c("02", "15", "60", "66", "69", "72", "78"),]
 us_aea <- rbind(us_aea, alaska, hawaii, puerto_rico)
 
+# Turn into something ggplot can use --------------------------------------
+
 map <- fortify(us_aea, region="STUSPS")
 
 by_state %>%
   count(STUSPS, wt=n) %>%
   select(id=STUSPS, n) -> state_totals
+
+
+# Plot animation panel by year using bars for cumulative data -------------
 
 i <- 0
 
@@ -58,6 +76,8 @@ for (YEAR in sort(unique(by_state$year))) {
   by_state %>%
     filter(year==YEAR) %>%
     select(id=STUSPS, n) -> awards
+
+# Make the map ------------------------------------------------------------
 
   gg <- ggplot()
   gg <- gg + geom_map(data=map, map=map,
@@ -71,26 +91,33 @@ for (YEAR in sort(unique(by_state$year))) {
   gg <- gg + coord_equal()
   gg <- gg + theme_map()
   gg <- gg + theme(legend.position="right")
-  gg <- gg + theme(plot.title=element_text(size=16))
+  gg <- gg + theme(plot.title=element_text(size=26))
   gg <- gg + theme(legend.position="top")
   choro <- gg
+
+# Make the bars -----------------------------------------------------------
 
   by_state %>%
     filter(year<=YEAR) %>%
     count(STUSPS, wt=n) %>%
     select(state=STUSPS, n) %>%
-    right_join(data.frame(state=state.abb)) %>%
-    mutate(n=ifelse(is.na(n), 0, n)) -> cumulative_awards
+    right_join(data.frame(state=state.abb, stringsAsFactors=FALSE), by="state") %>%
+    mutate(n=ifelse(is.na(n), 0, n),
+           col=factor(as.numeric(cut(n, breaks=c(0,30,60,90))))) -> cumulative_awards
 
   gg <- ggplot(data=cumulative_awards, aes(x=state, y=n))
-  gg <- gg + geom_bar(stat="identity", color="white", width=0.75, fill="#006d2c")
+  gg <- gg + geom_bar(stat="identity", aes(fill=col), color="#00441b", width=0.75, size=0.5)
   gg <- gg + scale_y_continuous(limits=c(0, 90))
+  gg <- gg + scale_fill_brewer(palette="BuGn")
   gg <- gg + labs(x=NULL, y=NULL, title=NULL)
   gg <- gg + theme_bw()
+  gg <- gg + theme(legend.position="none")
   gg <- gg + theme(panel.grid=element_blank())
   gg <- gg + theme(panel.border=element_blank())
   gg <- gg + theme(axis.text.x=element_text(size=7))
   bars <- gg
+
+# Output file -------------------------------------------------------------
 
   png(sprintf("output/panel-%03d.png", i), width=800, height=600)
   grid.arrange(choro, bars, ncol=1, heights=c(3,1))
@@ -99,6 +126,8 @@ for (YEAR in sort(unique(by_state$year))) {
   i <- i + 1
 
 }
+
+# Make the last panel -----------------------------------------------------
 
 by_state %>%
   count(STUSPS, wt=n) %>%
@@ -116,20 +145,23 @@ gg <- gg + labs(title="NSF 'Nuclear' Awards by State (Cumulative)")
 gg <- gg + coord_equal()
 gg <- gg + theme_map()
 gg <- gg + theme(legend.position="right")
-gg <- gg + theme(plot.title=element_text(size=16))
+gg <- gg + theme(plot.title=element_text(size=26))
 gg <- gg + theme(legend.position="top")
 choro <- gg
 
 state_totals %>%
   select(state=id, n) %>%
-  right_join(data.frame(state=state.abb)) %>%
-  mutate(n=ifelse(is.na(n), 0, n)) -> cumulative_awards
+  right_join(data.frame(state=state.abb, stringsAsFactors=FALSE), by="state") %>%
+  mutate(n=ifelse(is.na(n), 0, n),
+         col=factor(as.numeric(cut(n, breaks=c(0,30,60,90))))) -> cumulative_awards
 
 gg <- ggplot(data=cumulative_awards, aes(x=state, y=n))
-gg <- gg + geom_bar(stat="identity", color="white", width=0.75, fill="#006d2c")
+gg <- gg + geom_bar(stat="identity", aes(fill=col), color="#00441b", width=0.75, size=0.5)
 gg <- gg + scale_y_continuous(limits=c(0, 90))
+gg <- gg + scale_fill_brewer(palette="BuGn")
 gg <- gg + labs(x=NULL, y=NULL, title=NULL)
 gg <- gg + theme_bw()
+gg <- gg + theme(legend.position="none")
 gg <- gg + theme(panel.grid=element_blank())
 gg <- gg + theme(panel.border=element_blank())
 gg <- gg + theme(axis.text.x=element_text(size=7))
@@ -138,6 +170,8 @@ bars <- gg
 png(sprintf("output/panel-%03d.png", i), width=800, height=600)
 grid.arrange(choro, bars, ncol=1, heights=c(3,1))
 dev.off()
+
+# Convert to animated gif (needs ImageMagick) -----------------------------
 
 wd <- getwd()
 setwd("output/")
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Geospatial visualization for IU IV MOOC 2015 (Jan) - Assignment 03
+
+<img src='output/nuclear.gif'/>
